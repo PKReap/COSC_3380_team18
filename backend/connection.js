4234124cd371a2bd20ded3e76892eec4dcfd2e7f
Thien Pham <51933600+PKReap@users.noreply.github.com>
2022-03-05 16:46:01 -0600

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -1,26 +1,31 @@
 const mysql = require("mysql");
 
-mysqlConnection = mysql.createConnection({ // this is the connection to the database
+mysqlConnection = mysql.createConnection({
+  // this is the connection to the database
   host: "localhost",
   user: "root",
   password: "password",
   database: "musiclibrary",
 });
 
-mysqlConnection.connect((err) => { // connecting to the database
-  const messge = err ? `Error connecting to database: ${err.stack}` : `Connected to database`;
+mysqlConnection.connect((err) => {
+  // connecting to the database
+  const messge = err
+    ? `Error connecting to database: ${err.stack}`
+    : `Connected to database`;
   console.log(messge);
 });
 
-mysqlConnection.query("SELECT * FROM Users", (err, rows, fields) => { // querying to get all the users
-  if (!err) {
-    console.log(rows);
-  } else {
-    console.log(err);
-  }
+mysqlConnection.query("SELECT * FROM Users", (err, rows, fields) => {
+  // querying to get all the users
+  const messgage = err ? err : rows;
+  console.log(messgage);
 });
 
-mysqlConnection.end((err) => { // closing the connection
-  const messge = err ? `Error ending connection to database: ${err.stack}` : `Connection ended`;
+mysqlConnection.end((err) => {
+  // closing the connection
+  const messge = err
+    ? `Error ending connection to database: ${err.stack}`
+    : `Connection ended`;
   console.log(messge);
 });
