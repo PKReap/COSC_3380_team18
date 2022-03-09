a6b8e1206e4998d88011763b5059826d822d5430
Thien Pham <51933600+PKReap@users.noreply.github.com>
2022-03-09 17:07:11 -0600

    Update database-structure.sql

--- backend/dump/database-structure.sql
+++ backend/dump/database-structure.sql
@@ -3,28 +3,25 @@ CREATE SCHEMA IF NOT EXISTS MusicLibrary;
 USE MusicLibrary;
 
 CREATE TABLE
-    Admins (
-        PRIMARY KEY (AdminID),
-        AdminID INT NOT NULL AUTO_INCREMENT,
-        AdminName VARCHAR(20) NOT NULL UNIQUE
+    IF NOT EXISTS Users (
+        UserID INT NOT NULL AUTO_INCREMENT,
+        Username VARCHAR(50) NOT NULL,
+        UserPassword VARCHAR(50) NOT NULL,
+        PRIMARY KEY (UserID)
     );
 
-CREATE TABLE
-    Users (
-        PRIMARY KEY (UserID),
-        UserID INT NOT NULL AUTO_INCREMENT UNIQUE,
-        UserName VARCHAR(20) NOT NULL
-    );
 
 CREATE TABLE
-    Artists (
-        PRIMARY KEY (ArtistID, ArtistName),
-        ArtistID INT NOT NULL AUTO_INCREMENT,
-        ArtistName VARCHAR(20) NOT NULL UNIQUE
+    IF NOT EXISTS UserTypes (
+        PRIMARY KEY (UserTypeID),
+        UserTypeID INT NOT NULL AUTO_INCREMENT,
+        UserID INT NOT NULL,
+        UsersType ENUM("Admin", "Arist", "User") NOT NULL,
+        FOREIGN KEY (UserID) REFERENCES Users(UserID)        
     );
 
-CREATE TABLE
-    Playlists (
+CREATE TABLE 
+    IF NOT EXISTS Playlists (
         PRIMARY KEY (PlaylistID, PlaylistName),
         UserID INT NOT NULL,
         PlaylistID INT NOT NULL AUTO_INCREMENT,
@@ -33,17 +30,17 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    Libraries (
+    IF NOT EXISTS Libraries (
         PRIMARY KEY (LibraryID, LibraryName),
+        ArtistID INT NOT NULL,
         LibraryID INT NOT NULL AUTO_INCREMENT,
         LibraryName VARCHAR(20) NOT NULL,
-        ArtistID INT NOT NULL,
-        ArtistName VARCHAR(20) NOT NULL,
-        FOREIGN KEY (ArtistID, ArtistName) REFERENCES Artists(ArtistID, ArtistName)
+        FOREIGN KEY (ArtistID) REFERENCES Users(UserID)
     );
 
+
 CREATE TABLE
-    Tracks (
+    IF NOT EXISTS Tracks (
         PRIMARY KEY (TrackID, TrackName),
         TrackID INT NOT NULL AUTO_INCREMENT,
         TrackName VARCHAR(20) NOT NULL UNIQUE,
@@ -55,6 +52,10 @@ CREATE TABLE
         TrackGenre VARCHAR(20),
         LibraryID INT NOT NULL,
         LibraryName VARCHAR(20) NOT NULL,
+        Link VARCHAR(200) NOT NULL,
         FOREIGN KEY (LibraryID, LibraryName) REFERENCES Libraries(LibraryID, LibraryName),
-        FOREIGN KEY (ArtistID, ArtistName) REFERENCES Artists(ArtistID, ArtistName)
+        FOREIGN KEY (ArtistID) REFERENCES Users(UserID)
     );
+
+-- make link 300 characters long
+Alter table Tracks modify Link VARCHAR(300);
