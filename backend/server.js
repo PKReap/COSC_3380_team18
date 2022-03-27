bb563aadefad2e8ee7185b0c2ba60d88ead52ba9
thien <thienpham0109@gmail.com>
2022-03-27 16:57:31 -0500

    forgot to install mysql

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,14 @@
 const http = require("http");
 const paths = require("./src/MySQLConnection");
 
-const host = "localhost";
-const port = 3000;
-
 const server = http.createServer((req, res) => {
   const path = req.url.slice(1);
-  const responseHandler = paths[path];
+  const responseHandler = paths[path]; //  here it geting the function
   if (responseHandler) {
     req.on("data", (data) => {
       const args = Object.values(JSON.parse(data),);
       responseHandler(...args,  req.method ,(result) => {
-        res.writeHead(200, { "Content-Type": "application/json" });
+        res.writeHead(200, { "Content-Type": "application/json" }); //  return type is a json object
         res.end(JSON.stringify(result));
       });
     });
@@ -21,6 +18,9 @@ const server = http.createServer((req, res) => {
     res.end();
   }
 });
+
+const host = "localhost";
+const port = 3000;
 
 server.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}/`);
