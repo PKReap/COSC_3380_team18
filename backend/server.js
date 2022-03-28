8922e23acdc7aab368605b78774730d917988fa9
thien <thienpham0109@gmail.com>
2022-03-27 20:28:04 -0500

    better handles unknown paths

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ const server = http.createServer((req, res) => {
     });
   }
   else{
-    res.writeHead(404);
-    res.end();
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "Not found" }));
   }
 });
 
