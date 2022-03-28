43d4840de301f74cf49a662cd30cf1c941dedef2
thien <thienpham0109@gmail.com>
2022-03-27 21:15:37 -0500

    getting pass cors policy

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,28 @@
 const http = require("http");
 const paths = require("./src/MySQLConnection");
 
+
 const server = http.createServer((req, res) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+    "Access-Control-Max-Age": 2592000, // 30 days
+    "Content-Type": "application/json",
+  };
   const path = req.url.slice(1);
+  console.log(path);
   const responseHandler = paths[path]; //  here it geting the function
   if (responseHandler) {
     req.on("data", (data) => {
-      const args = Object.values(JSON.parse(data),);
-      responseHandler(...args,  req.method ,(result) => {
-        res.writeHead(200, { "Content-Type": "application/json" }); //  return type is a json object
+      const args = Object.values(JSON.parse(data));
+      console.log(args);
+      responseHandler(...args, req.method, (result) => {
+        res.writeHead(200, headers); //  return type is a json object
         res.end(JSON.stringify(result));
       });
     });
-  }
-  else{
-    res.writeHead(404, { "Content-Type": "application/json" });
+  } else {
+    res.writeHead(404, headers);
     res.end(JSON.stringify({ error: "Not found" }));
   }
 });
