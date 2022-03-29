36ac00e817c3c7b85e00564df823b5d97f799746
thien <thienpham0109@gmail.com>
2022-03-29 08:01:38 -0500

    client.js

--- frontend/client.js
+++ frontend/client.js
@@ -14,3 +14,49 @@ function validateUser(username, password) {
   });
   information.innerHTML = "validating...";
 }
+
+// render navbar
+
+function createElement(tag, params) {
+  const element = document.createElement(tag);
+  const keys = Object.keys(params);
+
+  keys.forEach((key) => {
+    element[key] = params[key];
+  });
+  return element;
+}
+
+function combine(elements) {
+  const Objects = Object.values(elements);
+  const parent = Objects[0];
+  for (let i = 1; i < Objects.length; i++) {
+    parent.appendChild(Objects[i]);
+  }
+  console.log(parent);
+  return parent;
+}
+
+function createNavbar() {
+  const navbar = {
+    div: createElement("div", { className: "topnav", id: "navbar" }),
+    home: createElement("a", {
+      className: "active",
+      href: "#home",
+      innerHTML: "Home",
+    }),
+    login: createElement("a", { href: "#login", innerHTML: "Login" }),
+    register: createElement("a", { href: "#Register", innerHTML: "Register" }),
+  };
+  const nav = combine(navbar);
+  return nav;
+}
+
+const pageView = {
+  navbar: createNavbar(),
+};
+
+function render(page) {
+  const view = combine(page);
+  document.body.appendChild(view);
+}
