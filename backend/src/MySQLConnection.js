711ac128728dbe6ec5852d9c6d291deb8426ced0
thien <thienpham0109@gmail.com>
2022-03-28 21:03:11 -0500

    password strength and length

--- backend/src/MySQLConnection.js
+++ backend/src/MySQLConnection.js
@@ -6,6 +6,18 @@ function validateUser(username, password, method, callback) {
     callback({ error: "Invalid username or password" });
     return;
   }
+  if (password.length < 8) {
+    callback({ error: "Password must be at least 8 characters" });
+    return;
+  }
+  if (!password.match(/[A-Z]/) || !password.match(/[0-9]/)) {
+    callback({
+      error:
+        "Password must contain at least one uppercase letter and one number",
+    });
+    return;
+  }
+
   const sql = `SELECT * FROM Users WHERE Username = '${username}' AND UserPassword = '${password}'`;
   query(sql, (result) => {
     const response = {
@@ -22,7 +34,5 @@ function getAllUsers(method, callback) {
     callback(result);
   });
 }
-
-
 
 module.exports = { validateUser, getAllUsers };
