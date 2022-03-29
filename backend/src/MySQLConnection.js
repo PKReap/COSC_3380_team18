eaa5a0047b2557a7385432fdac9add0717e881d8
thien <thienpham0109@gmail.com>
2022-03-28 22:01:07 -0500

    made cleaning make more sense

--- backend/src/MySQLConnection.js
+++ backend/src/MySQLConnection.js
@@ -1,22 +1,6 @@
 const { query } = require("./connection");
 
 function validateUser(username, password, callback) {
-  if ((username + password).match(/[^a-zA-Z0-9]/)) {
-    callback({ error: "Invalid username or password" });
-    return;
-  }
-  if (password.length < 8) {
-    callback({ error: "Password must be at least 8 characters" });
-    return;
-  }
-  if (!password.match(/[A-Z]/) || !password.match(/[0-9]/)) {
-    callback({
-      error:
-        "Password must contain at least one uppercase letter and one number",
-    });
-    return;
-  }
-
   const sql = `SELECT * FROM Users WHERE Username = '${username}' AND UserPassword = '${password}'`;
   query(sql, (result) => {
     const response = {
@@ -37,6 +21,21 @@ function getAllUsers(callback) {
 }
 
 function registerUser(username, password, callback) {
+  if ((username + password).match(/[^a-zA-Z0-9]/)) {
+    callback({ error: "Invalid username or password" });
+    return;
+  }
+  if (password.length < 8) {
+    callback({ error: "Password must be at least 8 characters" });
+    return;
+  }
+  if (!password.match(/[A-Z]/) || !password.match(/[0-9]/)) {
+    callback({
+      error:
+        "Password must contain at least one uppercase letter and one number",
+    });
+    return;
+  }
   const checkUserName = `SELECT * FROM Users WHERE Username = '${username}'`;
   query(checkUserName, (result) => {
     if (result.length > 0) {
