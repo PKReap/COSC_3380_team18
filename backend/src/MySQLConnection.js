7d616c0c783128e424364b5f60fb3541e7111bf5
thien <thienpham0109@gmail.com>
2022-03-30 18:04:12 -0500

    a more templetized function standard

--- backend/src/MySQLConnection.js
+++ backend/src/MySQLConnection.js
@@ -1,12 +1,13 @@
 const { query } = require("./connection");
 
-function validateUser(username, password, callback) {
-  if ((username + password).match(/[^a-zA-Z0-9]/)) {
+function validateUser(args , callback) {
+  const { Username, UserPassword } = args;
+  if ((Username + UserPassword).match(/[^a-zA-Z0-9]/)) {
     // cleaning the username and password against SQL injection
     callback({ error: "Invalid username or password" });
     return;
   }
-  const sql = `SELECT * FROM Users WHERE Username = '${username}' AND UserPassword = '${password}'`;
+  const sql = `SELECT * FROM Users WHERE Username = '${Username}' AND UserPassword = '${UserPassword}'`;
   query(sql, (result) => {
     const response = {
       validation: result.length > 0,
@@ -15,7 +16,7 @@ function validateUser(username, password, callback) {
   });
 }
 
-function getAllUsers(callback) {
+function getAllUsers(args ,callback) {
   const sql = "SELECT * FROM Users";
   query(sql, (result) => {
     const response = {
@@ -25,36 +26,37 @@ function getAllUsers(callback) {
   });
 }
 
-function registerUser(username, password, callback) {
-  if ((username + password).match(/[^a-zA-Z0-9]/)) {
+function registerUser(args, callback) {
+  const { Username, UserPassword } = args;
+
+  if ((Username + UserPassword).match(/[^a-zA-Z0-9]/)) {
     callback({ error: "Invalid username or password" });
     return;
   }
-  if (password.length < 8) {
+  if (UserPassword.length < 8) {
     callback({ error: "Password must be at least 8 characters" });
     return;
   }
-  if (!password.match(/[A-Z]/) || !password.match(/[0-9]/)) {
+  if (!UserPassword.match(/[A-Z]/) || !UserPassword.match(/[0-9]/)) {
     callback({
       error:
         "Password must contain at least one uppercase letter and one number",
     });
     return;
   }
-  const checkUserName = `SELECT * FROM Users WHERE Username = '${username}'`;
+  const checkUserName = `SELECT * FROM Users WHERE Username = '${Username}'`;
   query(checkUserName, (result) => {
     if (result.length > 0) {
       callback({ error: "Username already exists" });
       return;
     }
-        
-    const sql = `INSERT INTO Users (Username, UserPassword) VALUES ('${username}', '${password}')`;
+
+    const sql = `INSERT INTO Users (Username, UserPassword) VALUES ('${Username}', '${UserPassword}')`;
     query(sql, (error, result) => {
       if (error) callback({ error: "Error registering user" });
       else callback({ success: "User registered successfully" });
     });
   });
 }
-
 
 module.exports = { validateUser, getAllUsers, registerUser };
