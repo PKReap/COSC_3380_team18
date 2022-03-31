726de8552e2d90cae33ba9183e7581e241922afb
Thien <Thienpham0109@gmail.com>
2022-03-31 10:39:43 -0500

    up to date with live

--- frontend/client.js
+++ frontend/client.js
@@ -1,60 +1,53 @@
+function createElement(tag, params) {
+  const element = document.createElement(tag);
+  const keys = Object.keys(params);
+  keys.forEach((key) => {
+    element[key] = params[key];
+  });
+  return element;
+}
 
-  function createElement(tag, params) {
-    const element = document.createElement(tag);
-    const keys = Object.keys(params);
+function validateUser(username, password) {
+  const information = document.getElementById("information");
+  $.ajax({
+    url: "http://uhmusic.xyz/api/validateUser",
+    type: "POST",
+    data: JSON.stringify({ username, password }),
+    success: (data) => {
+      const validation = data.validation;
+      information.innerHTML = JSON.stringify(validation);
+    },
+    error: (err) => {
+      information.innerHTML = JSON.stringify(err);
+    },
+  });
+  information.innerHTML = "validating...";
+}
 
-    keys.forEach((key) => {
-      element[key] = params[key];
-    });
-    return element;
-  }
-
-
-  function validateUser(username, password) {
-    const information = document.getElementById("information");
-    $.ajax({
-      url: "http://uhmusic.xyz/api/validateUser",
-      type: "POST",
-      data: JSON.stringify({ username, password }),
-      success: (data) => {
-        const validation = data.validation;
-        information.innerHTML = JSON.stringify(validation);
-      },
-      error: (err) => {
-        information.innerHTML = JSON.stringify(err);
-      },
-    });
-    information.innerHTML = "validating...";
-  }
-
-  function getAllUsers(){
-    const element = document.getElementById("UserTable");
-    $.ajax({
-      url: "http://uhmusic.xyz/api/getAllUsers",
-      type: "POST",
-      data: JSON.stringify({}),
-      success: (data) => {
-        const { users } = data;
-        element.innerHTML = "Name | ID | Type"
-        users.forEach((user) => {
-          const { Username, UserID , UserType } = user;
-          const userElement = createElement("li", { innerHTML: `${Username} - ${UserID} - ${UserType}` });
-          element.appendChild(userElement);
+function getAllUsers() {
+  const element = document.getElementById("UserTable");
+  $.ajax({
+    url: "http://uhmusic.xyz/api/getAllUsers",
+    type: "POST",
+    data: JSON.stringify({}),
+    success: (data) => {
+      const { users } = data;
+      users.forEach((user) => {
+        const { Username, UserID, UserType } = user;
+        const row = createElement("tr", {});
+        const columns = [
+          createElement("td", { innerHTML: UserID }),
+          createElement("td", { innerHTML: Username }),
+          createElement("td", { innerHTML: UserType }),
+        ];
+        columns.forEach((column) => {
+          row.appendChild(column);
         });
-      },
-      error: (err) => {
-        element.innerHTML = JSON.stringify(err);
-      },
-    });
-    element.innerHTML = "getting users...";
-  }
-
-
-  function saveInformation(){
-    localStorage.setItem("currentUser", 15);
-  }
-
-  function getInformation(){
-    const currentUser = localStorage.getItem("currentUser");
-    console.log(currentUser);
-  }
+        element.appendChild(row);
+      });
+    },
+    error: (err) => {
+      element.innerHTML = JSON.stringify(err);
+    },
+  });
+}
