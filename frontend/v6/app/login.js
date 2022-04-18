46d98ab000641af70f87cb332fa71e99723e3b38
ninjaX172 <98909274+ninjaX172@users.noreply.github.com>
2022-04-18 16:41:59 -0500

    Add files via upload

---
+++ frontend/v6/app/login.js
@@ -0,0 +1,65 @@
+const form = document.getElementById("login-form");
+
+homepagePath = "";
+
+if (
+    localStorage.getItem("uh-music-username") != null &&
+    localStorage.getItem("uh-music-password") != null &&
+    localStorage.getItem("uh-music-userType") != null
+) {
+    var username = localStorage.getItem("uh-music-username");
+    var password = localStorage.getItem("uh-music-password");
+    var userType = localStorage.getItem("uh-music-userType");
+
+    console.log(username);
+
+    fetch("https://uhmusic.xyz/api/validateUser", {
+        method: "POST",
+        headers: { "Content-Type": "text/plain" },
+        body: `{\r\n    \"username\": \"${username}\",\r\n    \"password\": \"${password}\"\r\n}`,
+        redirect: "follow",
+    })
+        .then((response) => response.text())
+        .then((data) => {
+            data = JSON.parse(data);
+            console.log(data);
+            if (data.validation == true) {
+                document.location = "homepage.html";
+            }
+        })
+        .catch((error) => console.log("error", error));
+    console.log("Hello");
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    username = document.getElementById("username");
+    password = document.getElementById("password");
+    fetch("https://uhmusic.xyz/api/validateUser", {
+        method: "POST",
+        headers: { "Content-Type": "text/plain" },
+        body: `{\r\n    \"username\": \"${username.value}\",\r\n    \"password\": \"${password.value}\"\r\n}`,
+        redirect: "follow",
+    })
+        .then((response) => response.text())
+        .then((data) => {
+            // {"validation":true,"message":"User logged in successfully","username":"Artist1","userType":"Artist"}
+            data = JSON.parse(data);
+            console.log(data);
+            if (data.validation == true) {
+                localStorage.setItem("uh-music-username", username.value);
+                localStorage.setItem("uh-music-userType", data.userType);
+                localStorage.setItem("uh-music-password", password.value);
+
+                if (data.userType == "Admin") {
+                    document.location = ''  //<--- Admin page
+                } else {
+                    document.location = "homepage.html";
+                }
+            } else {
+                username.value = "";
+                password.value = "";
+            }
+        })
+        .catch((error) => console.log("error", error));
+});
